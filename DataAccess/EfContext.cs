2584759c079dd66a31f25785d688264fc3c2f7e1
Evgeny Prytkov <jeka.pratkov2@gmail.com>
2021-03-27 00:14:37 +0300

    Add DataAccess layer (Ef 6) + UnitTests

---
+++ DataAccess/EfContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace DataAccess
+{
+    public class EfContext : DbContext
+    {
+        public EfContext(string connection) : base(connection)
+        { }
+    }
+}
---
+++ DataAccess/EfContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.Infrastructure;
+
+namespace DataAccess
+{
+    public class EfContextFactory : IDbContextFactory<EfContext>
+    {
+        public EfContext Create()
+        {
+            return new EfContext("Addr=.;Database=HabitTracker;Trusted_Connection=True;");
+        }
+    }
+}
